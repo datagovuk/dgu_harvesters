b9591815656e7b9e5ab8aa1f6aef32099142c89c
Ross Jones <ross@servercode.co.uk>
2017-04-18 18:17:32 +0100

    Add setup and gitignore

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='DGU_Harvesters',
+    version='1.0',
+    description='Harvester library for data.gov.uk',
+    author='Data Discovery Team',
+    author_email='',
+    url='https://github.com/datagovuk/dgu_harvesters',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    install_requires=[
+    ]
+)
